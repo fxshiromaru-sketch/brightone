82fe4c8ed6d46968f23b2f493acd9973ee294405
brightone <test@gmail.com>
2026-07-12 18:30:13 +0900

    更新内容

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -8,10 +8,59 @@ export default function Dashboard() {
 
 const [cars, setCars] = useState<any[]>([]);
 const [purchaseCount, setPurchaseCount] = useState(0);
+const [recentPurchases, setRecentPurchases] = useState<any[]>([]);
 const [deleting, setDeleting] = useState(false);
 useEffect(() => {
 
   getCars();
+  async function getPurchaseData(){
+
+
+  const {
+    data,
+    error,
+    count
+  } = await supabase
+
+    .from("purchase_requests")
+
+    .select("*", {
+      count:"exact"
+    })
+
+    .order(
+      "created_at",
+      {
+        ascending:false
+      }
+    )
+    .limit(3);
+
+
+
+  if(error){
+
+    console.log(error);
+
+    return;
+
+  }
+
+
+
+  setPurchaseCount(
+    count || 0
+  );
+
+
+  setRecentPurchases(
+    data || []
+  );
+
+
+}
+  getPurchaseData();
+
   async function getPurchaseCount(){
 
   const {
@@ -235,7 +284,87 @@ className="inline-block bg-yellow-500 hover:bg-yellow-400 text-black font-bold p
 </Link>
 
 </div>
+<div className="mt-10">
 
+
+<h2 className="text-2xl font-bold mb-5">
+
+最近の査定依頼
+
+</h2>
+
+
+
+<div className="grid gap-5 md:grid-cols-3">
+
+
+{
+recentPurchases.map((item)=>(
+
+
+<Link
+
+key={item.id}
+
+href={`/admin/dashboard/purchase/${item.id}`}
+
+className="
+bg-zinc-900
+rounded-xl
+p-5
+hover:border
+hover:border-yellow-500
+transition
+"
+
+>
+
+
+<h3 className="text-xl font-bold">
+
+{item.car_name}
+
+</h3>
+
+
+
+<p className="text-zinc-400 mt-2">
+
+{item.name}
+
+</p>
+
+
+
+<span className="
+inline-block
+mt-3
+bg-yellow-500
+text-black
+px-3
+py-1
+rounded-full
+text-sm
+font-bold
+">
+
+{item.status}
+
+</span>
+
+
+</Link>
+
+
+))
+
+}
+
+
+</div>
+
+
+</div>
 <div className="mt-6 flex gap-4 flex-wrap">
 
 
