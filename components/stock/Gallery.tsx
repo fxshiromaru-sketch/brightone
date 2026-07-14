db9288913ae04b1522ef6930037669a9bc0aa95e
brightone <test@gmail.com>
2026-07-15 07:59:15 +0900

    更新内容

--- components/stock/Gallery.tsx
+++ components/stock/Gallery.tsx
@@ -10,6 +10,7 @@ export default function Gallery({ images }: Props) {
 
   const [current, setCurrent] = useState(0);
 
+
   if (!images || images.length === 0) {
 
     return (
@@ -20,45 +21,96 @@ export default function Gallery({ images }: Props) {
 
   }
 
+
   return (
 
     <div>
 
-      <img
-        src={images[current]}
-        className="w-full h-[520px] object-contain- rounded-xl"
-        alt=""
-      />
 
-      <div className="flex gap-3 mt-4 overflow-x-auto pb-2">
+      {/* メイン画像 */}
+
+      <div className="
+        w-full
+        h-[520px]
+        bg-zinc-900
+        rounded-2xl
+        overflow-hidden
+        flex
+        items-center
+        justify-center
+      ">
+
+
+        <img
+          src={images[current]}
+          className="
+            max-w-full
+            max-h-full
+            object-contain
+          "
+          alt=""
+        />
+
+
+      </div>
+
+
+
+
+      {/* サムネイル */}
+
+      <div className="
+        flex
+        gap-3
+        mt-4
+        overflow-x-auto
+        pb-2
+      ">
+
 
         {images.map((image, index) => (
 
-          <img
+
+          <div
             key={index}
-            src={image}
-            onClick={() => setCurrent(index)}
             className={`
-
-            w-28
-            h-20
-            object-cover
-            rounded-lg
-            cursor-pointer
-            border-2
-
-            ${
-              current === index
-                ? "border-yellow-500"
-                : "border-zinc-700"
-            }
-
+              flex-shrink-0
+              w-28
+              h-20
+              bg-zinc-900
+              rounded-lg
+              overflow-hidden
+              cursor-pointer
+              border-2
+              ${
+                current === index
+                  ? "border-yellow-500"
+                  : "border-zinc-700"
+              }
             `}
-          />
+          >
+
+
+            <img
+              src={image}
+              onClick={() => setCurrent(index)}
+              className="
+                w-full
+                h-full
+                object-contain
+              "
+              alt=""
+            />
+
+
+          </div>
+
 
         ))}
 
+
       </div>
+
 
     </div>
 
