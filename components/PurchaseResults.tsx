f072f6c3de751b41515ee93b802b3d0fed96d0ac
brightone <test@gmail.com>
2026-07-23 13:02:16 +0900

    更新内容

--- components/PurchaseResults.tsx
+++ components/PurchaseResults.tsx
@@ -10,7 +10,7 @@ export default async function PurchaseResults() {
     .order("created_at", {
       ascending: false,
     })
-    .limit(3);
+    .limit(6);
 
 
   if (!results || results.length === 0) {
